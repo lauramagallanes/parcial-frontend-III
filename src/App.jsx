233f6e194a8d786b244c7cffdeb5be05f944a8b1
laura magallanes <pasocenturiontours@gmail.com>
2024-11-04 20:56:55 -0300

    Primer commit - creacion de la app con vite y componente Card

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Card from './Card'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Carga de estudiantes</h1>
+        <form action=""></form>
+        <Card/>
+      </div>
+    </>
+  )
+}
+
+export default App
